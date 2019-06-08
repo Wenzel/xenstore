d816e75b759463bdc20aff3313a09a9426e3f127
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-06-08 19:49:20 +0200

    add directory

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,20 @@
 extern crate xenstore_sys;
 use std::io::Error;
 use std::ptr::{null_mut};
+use std::os::raw::{c_uint, c_char};
+use std::ffi::{CString};
+use std::slice;
+
+pub enum XBTransaction {
+    //Null = xenstore_sys::XBT_NULL,
+    Null = 0,
+}
+
+pub enum XsOpenFlags {
+    // ReadOnly = xenstore_sys::XS_OPEN_READONLY,
+    ReadOnly = 1,
+    SocketOnly = 2,
+}
 
 pub struct Xs {
     handle: *mut xenstore_sys::xs_handle,
@@ -8,10 +22,9 @@ pub struct Xs {
 
 impl Xs {
 
-    pub fn new() -> Result<Self,Error> {
+    pub fn new(open_type: XsOpenFlags) -> Result<Self,Error> {
         let xs_handle = unsafe {
-            let result = xenstore_sys::xs_open(xenstore_sys::XS_OPEN_READONLY.into());
-            result
+            xenstore_sys::xs_open(open_type as u64)
         };
         if xs_handle == null_mut() {
             return Err(Error::last_os_error());
@@ -19,6 +32,22 @@ impl Xs {
         return Ok(Xs {
             handle: xs_handle,
         });
+    }
+
+    pub fn directory(&self, transaction: XBTransaction, path: String) -> Vec<String> {
+        let num: *mut c_uint = null_mut();
+        let c_path = CString::new(path).unwrap();
+        let mut dir: Vec<String> = Vec::new();
+        unsafe {
+            println!("here");
+            let res = xenstore_sys::xs_directory(self.handle, transaction as u32, c_path.as_ptr(), num);
+            let array: &[*mut c_char] = slice::from_raw_parts_mut(res, *num as usize);
+            for x in array {
+                let s = CString::from_raw(*x).into_string().unwrap();
+                dir.push(s.clone());
+            }
+        }
+        dir
     }
 
     pub fn close(&mut self) {
