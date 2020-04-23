ca714adb4e75aba183eda9bcdd7690fdfb111d16
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-04-23 06:27:52 +0200

    load xenstore dynamically at runtime

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
-extern crate xenstore_sys;
+mod libxenstore;
+
+use std::ffi::{CStr, CString};
 use std::io::Error;
-use std::ptr::{null_mut};
-use std::os::raw::{c_uint, c_char};
-use std::ffi::{CString, CStr};
+use std::os::raw::{c_char, c_uint};
+use std::ptr::null_mut;
 use std::slice;
+
+use libxenstore::LibXenStore;
 
 #[repr(u32)]
 pub enum XBTransaction {
@@ -19,19 +22,19 @@ pub enum XsOpenFlags {
 #[derive(Debug)]
 pub struct Xs {
     handle: *mut xenstore_sys::xs_handle,
+    libxenstore: LibXenStore,
 }
 
 impl Xs {
-
-    pub fn new(open_type: XsOpenFlags) -> Result<Self,Error> {
-        let xs_handle = unsafe {
-            xenstore_sys::xs_open(open_type as u64)
-        };
+    pub fn new(open_type: XsOpenFlags) -> Result<Self, Error> {
+        let libxenstore = unsafe { LibXenStore::new() };
+        let xs_handle = (libxenstore.open)(open_type as u64);
         if xs_handle == null_mut() {
             return Err(Error::last_os_error());
         }
         return Ok(Xs {
             handle: xs_handle,
+            libxenstore,
         });
     }
 
@@ -40,8 +43,9 @@ impl Xs {
         let num_ptr: *mut c_uint = &mut num;
         let c_path = CString::new(path).unwrap();
         let mut dir: Vec<String> = Vec::new();
+        let res =
+            (self.libxenstore.directory)(self.handle, transaction as u32, c_path.as_ptr(), num_ptr);
         unsafe {
-            let res = xenstore_sys::xs_directory(self.handle, transaction as u32, c_path.as_ptr(), num_ptr);
             let array: &[*mut c_char] = slice::from_raw_parts_mut(res, num as usize);
             for x in array {
                 dir.push(CStr::from_ptr(*x).to_string_lossy().into_owned());
@@ -55,16 +59,17 @@ impl Xs {
         let mut len = 0;
         let len_ptr: *mut c_uint = &mut len;
         let c_path = CString::new(path).unwrap();
+        let res =
+            (self.libxenstore.read)(self.handle, transaction as u32, c_path.as_ptr(), len_ptr);
         unsafe {
-            let res = xenstore_sys::xs_read(self.handle, transaction as u32, c_path.as_ptr(), len_ptr);
-            CStr::from_ptr(res as *mut c_char).to_string_lossy().into_owned()
+            CStr::from_ptr(res as *mut c_char)
+                .to_string_lossy()
+                .into_owned()
         }
     }
 
     fn close(&mut self) {
-        unsafe {
-            xenstore_sys::xs_close(self.handle);
-        };
+        (self.libxenstore.close)(self.handle);
     }
 }
 
@@ -73,4 +78,3 @@ impl Drop for Xs {
         self.close();
     }
 }
-
---
+++ src/libxenstore.rs
@@ -0,0 +1,63 @@
+use std::os::raw::{c_char, c_uint, c_ulong, c_void};
+
+use xenstore_sys::{xs_handle, xs_transaction_t};
+
+use libloading::os::unix::Symbol as RawSymbol;
+use libloading::{Library, Symbol};
+use log::info;
+
+const LIBXENSTORE_FILENAME: &'static str = "libxenstore.so";
+// xs_open
+type FnOpen = fn(flags: c_ulong) -> *mut xs_handle;
+// xs_close
+type FnClose = fn(xsh: *mut xs_handle);
+// xs_directory
+type FnDirectory = fn(
+    h: *mut xs_handle,
+    t: xs_transaction_t,
+    path: *const c_char,
+    num: *mut c_uint,
+) -> *mut *mut c_char;
+// xs_read
+type FnRead = fn(
+    h: *mut xs_handle,
+    t: xs_transaction_t,
+    path: *const c_char,
+    len: *mut c_uint,
+) -> *mut c_void;
+
+#[derive(Debug)]
+pub struct LibXenStore {
+    lib: Library,
+    pub open: RawSymbol<FnOpen>,
+    pub close: RawSymbol<FnClose>,
+    pub directory: RawSymbol<FnDirectory>,
+    pub read: RawSymbol<FnRead>,
+}
+
+impl LibXenStore {
+    pub unsafe fn new() -> Self {
+        info!("Loading {}", LIBXENSTORE_FILENAME);
+        let lib = Library::new(LIBXENSTORE_FILENAME).unwrap();
+        // load symbols
+        let open_sym: Symbol<FnOpen> = lib.get(b"xs_open\0").unwrap();
+        let open = open_sym.into_raw();
+
+        let close_sym: Symbol<FnClose> = lib.get(b"xs_close\0").unwrap();
+        let close = close_sym.into_raw();
+
+        let directory_sym: Symbol<FnDirectory> = lib.get(b"xs_directory\0").unwrap();
+        let directory = directory_sym.into_raw();
+
+        let read_sym: Symbol<FnRead> = lib.get(b"xs_read\0").unwrap();
+        let read = read_sym.into_raw();
+
+        LibXenStore {
+            lib,
+            open,
+            close,
+            directory,
+            read,
+        }
+    }
+}
