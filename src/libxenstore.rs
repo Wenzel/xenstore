d38eb3113aff68586f24dedbfd8f2cd9828e0df8
Mathieu Tarral <mathieu.tarral@protonmail.com>
2023-10-25 01:38:28 +0200

    attempts multiple sonames when loading libxenstore

--- src/libxenstore.rs
+++ src/libxenstore.rs
@@ -7,7 +7,8 @@ use libloading::os::unix::Symbol as RawSymbol;
 use libloading::{Error, Library, Symbol};
 use log::info;
 
-const LIBXENSTORE_BASENAME: &str = "libxenstore.so.4";
+const LIBXENSTORE_SONAME_LIST: [&str; 2] = ["3.0", "4"];
+const LIBXENSTORE_BASENAME: &str = "libxenstore.so.";
 // xs_open
 type FnOpen = fn(flags: c_ulong) -> *mut xs_handle;
 // xs_close
@@ -49,37 +50,50 @@ pub struct LibXenStore {
 }
 
 impl LibXenStore {
+    /// Loads the libxenstore.so library dynamically, by trying multiple SONAMES
+    /// On failure it returns the last load error
     pub unsafe fn new() -> Result<Self, Error> {
-        let lib_filename: OsString = LIBXENSTORE_BASENAME.into();
-        info!("Loading {}", lib_filename.to_str().unwrap());
-        let lib = Library::new(lib_filename)?;
-        // load symbols
-        let open_sym: Symbol<FnOpen> = lib.get(b"xs_open\0")?;
-        let open = open_sym.into_raw();
+        let mut last_load_error: Option<Error> = None;
+        for soname in LIBXENSTORE_SONAME_LIST {
+            let lib_filename = format!("{}.{}", LIBXENSTORE_BASENAME, soname);
+            info!("Loading {}", lib_filename);
+            let result = Library::new::<OsString>(lib_filename.clone().into());
+            if let Err(err) = result {
+                info!("Failed to load {}", lib_filename);
+                last_load_error = Some(err);
+                continue;
+            } else {
+                let lib = result.unwrap();
+                // load symbols
+                let open_sym: Symbol<FnOpen> = lib.get(b"xs_open\0")?;
+                let open = open_sym.into_raw();
 
-        let close_sym: Symbol<FnClose> = lib.get(b"xs_close\0")?;
-        let close = close_sym.into_raw();
+                let close_sym: Symbol<FnClose> = lib.get(b"xs_close\0")?;
+                let close = close_sym.into_raw();
 
-        let directory_sym: Symbol<FnDirectory> = lib.get(b"xs_directory\0")?;
-        let directory = directory_sym.into_raw();
+                let directory_sym: Symbol<FnDirectory> = lib.get(b"xs_directory\0")?;
+                let directory = directory_sym.into_raw();
 
-        let read_sym: Symbol<FnRead> = lib.get(b"xs_read\0")?;
-        let read = read_sym.into_raw();
+                let read_sym: Symbol<FnRead> = lib.get(b"xs_read\0")?;
+                let read = read_sym.into_raw();
 
-        let rm_sym: Symbol<FnRm> = lib.get(b"xs_rm\0")?;
-        let rm = rm_sym.into_raw();
+                let rm_sym: Symbol<FnRm> = lib.get(b"xs_rm\0")?;
+                let rm = rm_sym.into_raw();
 
-        let write_sym: Symbol<FnWrite> = lib.get(b"xs_write\0")?;
-        let write = write_sym.into_raw();
+                let write_sym: Symbol<FnWrite> = lib.get(b"xs_write\0")?;
+                let write = write_sym.into_raw();
 
-        Ok(LibXenStore {
-            _lib: lib,
-            open,
-            close,
-            directory,
-            read,
-            rm,
-            write,
-        })
+                return Ok(LibXenStore {
+                    _lib: lib,
+                    open,
+                    close,
+                    directory,
+                    read,
+                    rm,
+                    write,
+                });
+            }
+        }
+        Err(last_load_error.unwrap())
     }
 }
